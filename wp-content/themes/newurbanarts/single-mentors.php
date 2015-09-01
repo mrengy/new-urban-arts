78ced9a3f0df691210dffe44c4c3965605ca76fc
Mike Eng <mike@buildium.com>
2015-09-01 10:49:58 -0400

    moving array manipulation before previous / next links

--- wp-content/themes/newurbanarts/single-mentors.php
+++ wp-content/themes/newurbanarts/single-mentors.php
@@ -23,23 +23,7 @@ get_header(); ?>
 			<div id="content" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
-					<?php
-					 	// only show previous / next navigation if this item is 'currently active'
-						if (is_array(get_post_custom_values('_current1'))){
-							if (in_array('yes', get_post_custom_values('_current1'))) { 
-					?>
-						<nav id="nav-single">
-							<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
-							<span class="nav-previous"><?php previous_post_link( '%link', __( 'Previous', 'twentyeleven' ) ); ?></span>
-							<!-- Restricted next link (next in same year) as per codex https://codex.wordpress.org/Template_Tags/next_post_link#Examples -->
-							<span class="nav-next">| <?php next_post_link( '%link', __( 'Next', 'twentyeleven' ), TRUE, '', 'years' ); ?></span>
 
-						</nav><!-- #nav-single -->
-					<?php 
-							} //end if current 
-						} //end if is array
-					?>
-					
 					<?php
 					 	//debugging custom fields
 						
@@ -49,8 +33,8 @@ get_header(); ?>
 							the_taxonomies( $post->ID ); 
 						echo '</strong>';
 						echo '<br />';
-						echo '<em>the_terms( $post->ID, "years")</em> ';
-						echo '<strong>';
+						echo '<em>arrays of the terms</em> ';
+						echo '<br />';
 							$nua_years = array( get_the_terms($post->ID, "years") );
 							print_r($nua_years);
 						echo '<br />';
@@ -68,23 +52,27 @@ get_header(); ?>
 								array_push($nua_previous_years, $a_year->name);
 							}
 							print_r($nua_previous_years);
-						/*
-						echo('<br />');
-							$nua_current_year = $nua_years[0][$nua_num_years - 1];
-							echo("current year = ");
-							print_r($nua_current_year->name);
-						*/
-						
-						echo '</strong>';
-						
-						// print_r(get_post_custom_keys($post->ID));
-						//print_r(get_post_custom_values('_current1'));
-						//print_r(get_post_custom($post->ID));
-						//print_r(get_post_meta($post->ID));
-						//the_meta();
 						echo '</pre>';
 						
 					?>
+					
+					<?php
+					 	// only show previous / next navigation if this item is 'currently active'
+						if (is_array(get_post_custom_values('_current1'))){
+							if (in_array('yes', get_post_custom_values('_current1'))) { 
+					?>
+						<nav id="nav-single">
+							<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>
+							<span class="nav-previous"><?php previous_post_link( '%link', __( 'Previous', 'twentyeleven' ) ); ?></span>
+							<!-- Restricted next link (next in same year) as per codex https://codex.wordpress.org/Template_Tags/next_post_link#Examples -->
+							<span class="nav-next">| <?php next_post_link( '%link', __( 'Next', 'twentyeleven' ), TRUE, '', 'years' ); ?></span>
+
+						</nav><!-- #nav-single -->
+					<?php 
+							} //end if current 
+						} //end if is array
+					?>
+
 					
 					<?php echo do_shortcode("[sce-get-featured]");?>
 					<?php get_template_part( 'content', 'single' ); ?>
